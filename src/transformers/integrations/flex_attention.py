500eaeb77d722d6a25cf07b729267b5de14154a5
Huaishun Hu <huaishun.hu@mthreads.com>
2025-12-16 11:46:28 +0800

    enable flex_decoding for MUSA

--- src/transformers/integrations/flex_attention.py
+++ src/transformers/integrations/flex_attention.py
@@ -73,10 +73,6 @@ class WrappedFlexAttention:
                 )
                 def compiled_flex_attention(*args, **kwargs):
                     kernel_options = kwargs.pop("kernel_options") or {}
-                    # FIXME: due to bug in triton_musa, flex decoding kernel will
-                    # cause triton compile errors, for now we just use flex attention
-                    kernel_options["FORCE_USE_FLEX_ATTENTION"] = \
-                        kernel_options.get("FORCE_USE_FLEX_ATTENTION", True)
                     kwargs["kernel_options"] = kernel_options
                     return __compiled_flex_attention(*args, **kwargs)
 
